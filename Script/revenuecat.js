6b3e82bcf751648cc25a4f775b652d01a7dec7f2
Toovooo <116278848+Toovooo@users.noreply.github.com>
2022-12-21 16:42:38 +0800

    Update revenuecat.js

--- Script/revenuecat.js
+++ Script/revenuecat.js
@@ -6,7 +6,9 @@ const list = {
 	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
 	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
 	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' },
-	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' }
+	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'ipTV': { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' },
+	'Speechify': { name: 'pro', id: 'com.cliffweitzman.speechifyMobile2.premium.annual' }
 };
 const data = {
 	"expires_date": "2099-02-18T07:52:54Z",
