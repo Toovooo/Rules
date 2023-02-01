ab860f6dff3d2fa2bbd79ef1695b9741e2e60b77
Toovooo <116278848+Toovooo@users.noreply.github.com>
2023-02-01 23:05:58 +0800

    Update revenuecat.js

--- Script/revenuecat.js
+++ Script/revenuecat.js
@@ -8,7 +8,10 @@ const list = {
 	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' },
 	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
 	'ipTV': { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' },
-	'Speechify': { name: 'pro', id: 'com.cliffweitzman.speechifyMobile2.premium.annual' }
+	'Speechify': { name: 'pro', id: 'com.cliffweitzman.speechifyMobile2.premium.annual' },
+	'Blink': { name: 'pro', id: 'blink_shell_plus_1y_1999' },
+        'mizframa': { name: 'premium', id: 'mf_20_lifetime2' },
+        'CallRecorder': { name: 'subscriptions', id: 'com.prettyboa.CallRecorder.MonthlySubscription2999' }
 };
 const data = {
 	"expires_date": "2099-02-18T07:52:54Z",
