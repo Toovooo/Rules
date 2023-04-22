72ae892890e199021c284942bfe19523163fe999
Toovooo <116278848+Toovooo@users.noreply.github.com>
2023-04-22 18:08:12 +0800

    Update startup.js

--- Script/startup.js
+++ Script/startup.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.url)) {
     let obj=JSON.parse($response.body);
     obj.data.materialList.startTime=3667476800000;
