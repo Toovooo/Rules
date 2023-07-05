6d7f74745e2f5bf2d706f151344ad1b16af94e33
Toovooo <116278848+Toovooo@users.noreply.github.com>
2023-07-05 19:56:04 +0800

    Update Bing.js

--- Script/Bing/Bing.js
+++ Script/Bing/Bing.js
@@ -1,22 +1,3 @@
-/*
-Bing积分-lowking-v2.3.5
-
-⚠️只测试过surge没有其他app自行测试
-1.3.4版本的速度更新，不然第二天无法重置执行状态，导致无法做任务
-记得到boxjs里面设置每日任务重置时间，不设置默认每天早上8点
-
-************************
-Surge 4.2.0+ 脚本配置(其他APP自行转换配置):
-************************
-
-[Script]
-# > Bing积分
-Bing积分cookie = requires-body=0,type=http-request,pattern=https:\/\/rewards\.bing\.com,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/bing/bingPoint.js
-Bing积分 = type=cron,cronexp="0 10 0 * * ?",wake-system=1,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/bing/bingPoint.js
-
-[MITM]
-hostname = %APPEND% rewards.bing.com
-*/
 const lk = new ToolKit(`Bing积分`, `BingPoint`, {"httpApi": "ffff@10.0.0.19:6166"})
 const scriptTimeout = 30
 const bingPointCookieKey = 'bingPointCookieKey'
@@ -117,7 +98,7 @@ if(!lk.isExecComm) {
             "keys": [bingPointCookieKey],
             "script_timeout": scriptTimeout
         }, {
-            "script_url": "https://github.com/Toovooo/Rules/blob/master/Script/bing.js",
+            "script_url": "https://github.com/Toovooo/Rules/blob/master/Script/Bing/Bing.js",
             "author": "@lowking",
             "repo": "https://github.com/lowking/Scripts",
         })
