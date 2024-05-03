909f2f72b6b2b4f8645e495c4ede2c37774bbb96
Toovooo <116278848+Toovooo@users.noreply.github.com>
2024-05-03 17:33:52 +0800

    Update appraven.js

--- Script/Adblock/appraven.js
+++ Script/Adblock/appraven.js
@@ -3,7 +3,7 @@ var body = $response.body;
 body = body.replace(/"premium":false/g, '"premium":true');
 body = body.replace(/"hasInAppPurchases":false/g,'"hasInAppPurchases":true');
 body = body.replace(/"youOwn":false/g,
-'"youOwn":true');
+'"youOwn":false');
 body = body.replace(/"arcade":false/g,
 '"arcade":true');
 
