ba98d7a5565aca6f8deb5e68ef7fe68d68792c06
Toovooo <116278848+Toovooo@users.noreply.github.com>
2026-07-05 22:36:09 +0800

    Delete Script/Pornhub directory

--- Script/Pornhub/pornhub.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Toovooo/Rules/master/Script/Pornhub/pornhub.css" type="text/css">');
-$done({ body });
